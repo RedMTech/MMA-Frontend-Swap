a6f6434923f81e85eeb2331e81fea49df0e710f2
RedMTech <97133401+RedMTech@users.noreply.github.com>
2022-01-18 10:50:55 +0100

    Update Global.ts

--- src/style/Global.ts
+++ src/style/Global.ts
@@ -2,8 +2,7 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background-color: ${({ theme }) => theme.colors.background};
-
+   background-color: #000000
     img {
       height: auto;
       max-width: 100%;
